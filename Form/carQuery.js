39fa8229e274e6855801b25f3f54766492fdc1b8
Jamie Tater <jtater1@gmail.com>
2020-05-26 00:25:50 -0400

    added quote form

---
+++ Form/carQuery.js
@@ -0,0 +1,13 @@
+$(document).ready(
+function()
+{
+     //Create a variable for the CarQuery object.  You can call it whatever you like.
+     var carquery = new CarQuery();
+
+     //Run the carquery init function to get things started:
+     carquery.init();
+     
+
+     //Optional: initialize the year, make, model, and trim drop downs by providing their element IDs
+     carquery.initYearMakeModelTrim('car-years', 'car-makes', 'car-models', 'car-model-trims');
+});
